3a2288f2994130f2d84ff51186ae0e5bb6f68f9a
admin <admin@pandasportal.net>
2012-08-13 07:40:57 +0000

    add getters and setters to cage

--- jcu-zoo/src/model/Cage.java
+++ jcu-zoo/src/model/Cage.java
@@ -14,11 +14,70 @@ public class Cage {
     private ArrayList<Gate> gates = new ArrayList<Gate>();
     private int cageId;
     private String cageName;
-    private int cageLocation;
-    private int cageDimensions;
-    private String cageType;
+    private Float latitude;
+    private Float longitude;
+    private Integer typeId;
     private boolean hasHuman;
     private boolean hasAnimal;
+    public Cage(int cageId, String cageName, Float latitude, Float longitude, Integer typeId, boolean hasHuman, boolean hasAnimal){
+        this.cageId = cageId;
+        this.cageName = cageName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.typeId = typeId;
+        this.hasHuman = hasHuman;
+        this.hasAnimal = hasAnimal;
+    }
+    public int getCageId(){
+        return cageId;
+    }
+    public String getCageName() {
+        return cageName;
+    }
+
+    public void setCageName(String cageName) {
+        this.cageName = cageName;
+    }
+
+    public boolean hasAnimal() {
+        return hasAnimal;
+    }
+
+    public void hasAnimal(boolean hasAnimal) {
+        this.hasAnimal = hasAnimal;
+    }
+
+    public boolean hasHuman() {
+        return hasHuman;
+    }
+
+    public void hasHuman(boolean hasHuman) {
+        this.hasHuman = hasHuman;
+    }
+
+    public Float getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Float latitude) {
+        this.latitude = latitude;
+    }
+
+    public Float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Float longitude) {
+        this.longitude = longitude;
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
     public void setNumOfDoors(int numGates){
         int diff = numGates - this.gates.size();
          if (diff > 0){
@@ -42,6 +101,17 @@ public class Cage {
         for (int i = numGates; i > 0; i--){
             this.gates.add(new Gate());
         }
+    }
+    
+    @Override
+    public String toString(){
+        return Integer.toString(this.cageId) + 
+                ", " + this.cageName + 
+                ", Lat: " + Float.toString(this.latitude) +
+                ", Long: " + Float.toString(this.longitude) +
+                ", TypeID: " + Integer.toString(this.typeId) +
+                ", hasAnimal: " + Boolean.toString(hasAnimal) +
+                ", hasHuman: " + Boolean.toString(hasHuman);
     }
     
     
