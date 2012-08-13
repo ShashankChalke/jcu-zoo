b99a70126afa67a561ea92e264059857cb7294ee
admin@pandasportal.net <admin@pandasportal.net@c42880af-f1a8-9d04-b8e8-8851d32fd19a>
2012-08-13 07:40:31 +0000

    update to work with the data access object

--- jcu-zoo/src/adminUI/AdminDAO.java
+++ jcu-zoo/src/adminUI/AdminDAO.java
@@ -98,14 +98,30 @@ public class AdminDAO implements IAdminDAO{
 
     @Override
     public void updateCage(Cage cage) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try {
+            connect();
+            query = "UPDATE CAGE SET CAGE_NAME=?, LATITUDE=?, LONGITUDE=?, TYPE_ID=?, HAS_HUMAN=?, HAS_ANIMAL=? WHERE CAGE_ID=?";        
+            preparedStatement = (PreparedStatement) conn.prepareStatement(query);
+            preparedStatement.setString(1,cage.getCageName());
+            preparedStatement.setFloat(2,cage.getLatitude());
+            preparedStatement.setFloat(3, cage.getLongitude());
+            preparedStatement.setInt(4,cage.getTypeId());
+            preparedStatement.setBoolean(5, cage.hasHuman());
+            preparedStatement.setBoolean(6, cage.hasAnimal());
+            preparedStatement.setInt(7, cage.getCageId());
+            preparedStatement.executeUpdate();
+        } catch (Exception ex){
+            Logger.getLogger(AdminDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            cleanUp();
+        }
     }
 
     @Override
     public void addCage() {
         try {
             connect();
-            query = "INSERT INTO CAGE() VALUES ()";        
+            query = "INSERT INTO CAGE(HAS_HUMAN,HAS_ANIMAL) VALUES (False,False)";        
             preparedStatement = (PreparedStatement) conn.prepareStatement(query);
             preparedStatement.executeUpdate();
         } catch (Exception ex){
@@ -131,16 +147,39 @@ public class AdminDAO implements IAdminDAO{
 
     @Override
     public ArrayList<Cage> getCages() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        //throw new UnsupportedOperationException("Not supported yet.");
+        ArrayList<Cage> cages = new ArrayList<Cage>();
+         try {
+            connect();
+            query = "SELECT * FROM CAGE";
+            preparedStatement = (PreparedStatement) conn.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                cages.add(new Cage(resultSet.getInt("CAGE_ID"),
+                        (String) resultSet.getString("CAGE_NAME"),
+                        (Float) resultSet.getFloat("LATITUDE"),
+                        (Float) resultSet.getFloat("LONGITUDE"),
+                        (Integer) resultSet.getInt("TYPE_ID"),
+                        (Boolean) resultSet.getBoolean("HAS_HUMAN"),
+                        (Boolean) resultSet.getBoolean("HAS_ANIMAL")));
+            }
+        } catch (Exception ex){
+            Logger.getLogger(AdminDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            cleanUp();
+        }        
+         return cages;
     }
     public static void main(String[] args) {
         Configuration.loadConfigurationFromFile();
         IAdminDAO adminDAO1 = AdminDAO.getInstanceOf();
         System.out.println(adminDAO1.getNumOfCages());
         adminDAO1.addCage();
+        adminDAO1.addCage();
         System.out.println(adminDAO.getNumOfCages());
         adminDAO1.removeLastCage();
         System.out.println(adminDAO1.getNumOfCages());
+        System.out.println(adminDAO1.getCages().toString());
     }
 
 }
