909cbe636f20969afbe2854094c9d5a4da27429d
admin@pandasportal.net <admin@pandasportal.net@c42880af-f1a8-9d04-b8e8-8851d32fd19a>
2012-08-20 21:18:02 +0000

    Admin data access object, cage data transfer object, a mysql connection class and testing page :D

--- jcu-zoo-php/index.php
+++ jcu-zoo-php/index.php
@@ -1,12 +1,48 @@
 <!DOCTYPE html>
+<?php include('class/AdminDAO.php');
+$admin = new AdminDAO();
+class AdminUI{
+    private $admin;
+    
+    public function __construct(){
+        $this->admin = new AdminDAO();
+    }
+    function listCages(){
+        echo("<table>");
+        foreach ($this->admin->getCages() as $cage){
+            echo("<tr>");
+            echo ("<td>".$cage->getCageId()."</td>");
+            echo ("<td>".$cage->getCageName()."</td>");
+            echo("</tr>");
+        }
+        echo("</table>");
+       
+    }
+}
+
+
+
+
+?>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
     </head>
     <body>
-        <?php
-        // put your code here
+        
+        <h1>Hello, World!</h1>
+        <a href="cage.php">CageUI</a>
+        <a href="admin.php">AdminUI</a>
+        <?php 
+        //echo(print_r($admin->getCages()));
+        //echo(print_r($admin->getCages()));
+        //echo("<br/>");
+        //echo(print_r($admin->getCage(37)));
+          $adminUI = new AdminUI();
+          $adminUI->listCages();
+
+             
         ?>
     </body>
 </html>
